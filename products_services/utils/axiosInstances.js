10d5b0b4a26a868aeda329fa1ea611fb78055431
Dileepa <113455403+dileepa200108@users.noreply.github.com>
2025-04-22 18:59:16 +0530

    k8 configured

--- products_services/utils/axiosInstances.js
+++ products_services/utils/axiosInstances.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an instance of axios for the authentication service
 const authService = axios.create({
-  baseURL: process.env.AUTH_SERVICE_URL || "http://localhost:3000", 
+  baseURL: process.env.AUTH_SERVICE_URL || "http://auth-service:3000", 
   withCredentials: true, 
   timeout: 10000, // Set a timeout of 10 seconds for requests
 });
