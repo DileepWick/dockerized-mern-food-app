e4f5ed1445b605bc501bbc40491fc4d76ac5ef8b
TharinduAri <tharindulakshita2001@gmail.com>
2025-04-22 21:05:07 +0530

    update

---
+++ delivery_service/config/db.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const dbURI = process.env.MONGO_URI;
+    if (!dbURI) {
+      console.error("MongoDB URI not defined!");
+      return;
+    }
+
+    await mongoose.connect(dbURI); // No need for useNewUrlParser and useUnifiedTopology anymore
+
+    console.log("Delivery Service MongoDB Connected Successfully! ✅");
+  } catch (err) {
+    console.error(`❌ MongoDB Connection Failed: ${err.message}`);
+  }
+};
+
+export default connectDB;
---
+++ delivery_service/index.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB  from './config/db.js'; // Import your DB connection module
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+
+import productRoutes from './routes/product_routes.js'; // Import your product routes
+
+
+// Load environment variables
+dotenv.config();
+
+
+// Initialize app
+const app = express();
+
+// Middleware to parse cookies
+app.use(cookieParser());
+
+
+// Enable CORS
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Allows only frontend running on localhost:5173
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
+    credentials: true, // Allow credentials (cookies) to be sent
+  })
+)
+
+// Middleware
+app.use(express.json());
+
+
+app.get('/', (req, res) => res.send('Hello from Product Service! ✅'));
+
+// MongoDB connection
+connectDB(); // Use the function from your db.js to connect to MongoDB
+
+// Routes
+app.use('/api/products', productRoutes); // Use product routes
+
+// Start server
+app.listen(process.env.PORT, () => {
+  console.log(`Delivery Service running on http://localhost:${process.env.PORT} ✅`);
+});
