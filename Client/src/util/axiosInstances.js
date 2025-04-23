31fabdbd98a2fa0255aa8ade9cb8828d710f8334
Dileepa <113455403+dileepa200108@users.noreply.github.com>
2025-04-23 14:17:49 +0530

    updated

--- Client/src/util/axiosInstances.js
+++ Client/src/util/axiosInstances.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance of axios for the authentication service
 const authService = axios.create({
-  baseURL: 'http://localhost:30000/api', // Change this to your backend URL
+  baseURL: 'http://localhost:30000/api', // Change this to your backend URL //Change this to 3000 fro when dev
   withCredentials: true, // Ensures cookies (JWT) are sent with requests
 });
 
@@ -12,7 +12,7 @@ const productsService = axios.create({
 });
 
 const restaurantService = axios.create({
-  baseURL: 'http://localhost:30001/api',
+  baseURL: 'http://localhost:30001/api', //Change this to 3001 fro when dev
   withCredentials: true,
 });
 
