06a4939da6720b7440ce4221cdf40189a42acc4c
Dileepa <113455403+dileepa200108@users.noreply.github.com>
2025-04-23 13:30:21 +0530

    updated ports

--- Client/src/util/axiosInstances.js
+++ Client/src/util/axiosInstances.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance of axios for the authentication service
 const authService = axios.create({
-  baseURL: 'http://localhost:3000/api', // Change this to your backend URL
+  baseURL: 'http://auth-service:3000/api', // Change this to your backend URL
   withCredentials: true, // Ensures cookies (JWT) are sent with requests
 });
 
