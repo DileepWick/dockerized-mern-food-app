ffa8af8106f02065c582d16ed9d8177ebb9a3b46
Dileepa <113455403+dileepa200108@users.noreply.github.com>
2025-04-23 14:08:18 +0530

    docker connected

--- Client/src/util/axiosInstances.js
+++ Client/src/util/axiosInstances.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance of axios for the authentication service
 const authService = axios.create({
-  baseURL: 'http://auth-service:3000/api', // Change this to your backend URL
+  baseURL: 'http://localhost:30000/api', // Change this to your backend URL
   withCredentials: true, // Ensures cookies (JWT) are sent with requests
 });
 
@@ -12,7 +12,7 @@ const productsService = axios.create({
 });
 
 const restaurantService = axios.create({
-  baseURL: 'http://restaurant-service:3001/api',
+  baseURL: 'http://localhost:30001/api',
   withCredentials: true,
 });
 
