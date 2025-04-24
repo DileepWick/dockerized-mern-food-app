0a18ad4ce9e9dabf0342f1913ebff0e48911a5c2
Dileepa <113455403+dileepa200108@users.noreply.github.com>
2025-04-24 11:52:04 +0530

    updated

--- product-service/routes/product_routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createProduct } from "../controllers/product_controller.js";
-
-
-const router = express.Router();
-
-router.post("/products",createProduct);
-
-export default router;
