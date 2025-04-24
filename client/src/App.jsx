5888af65fd4afb474dd95076a52bf9fc62c26096
Dasun Dushmantha <dasunathauda99@gmail.com>
2025-04-24 23:30:50 +0530

    controller edit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -43,7 +43,7 @@ function App() {
         <Route path='/register' element={<RegisterPage />} />
         <Route path='/restaurantForm' element={<RestaurantForm />} />
         <Route path='/sellerDashboard' element={<SellerDashboard />} />
-
+        
         {/* Protected Route */}
         <Route
           path='/'
