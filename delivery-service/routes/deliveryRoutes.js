5b21abd661edf3beb3a8af8672fce558f99abb03
TharinduAri <tharindulakshita2001@gmail.com>
2025-04-26 13:17:05 +0530

    updated delivery service

--- delivery-service/routes/deliveryRoutes.js
+++ delivery-service/routes/deliveryRoutes.js
@@ -1,22 +1,10 @@
 import express from 'express';
 import {
-  createDelivery,
-  getDeliveryById,
-  getDeliveryByOrderId,
-  getDriverDeliveries,
-  updatePickupTime,
-  completeDelivery,
-  reassignDriver
+  createDelivery
 } from '../controllers/deliveryController.js';
 
 const router = express.Router();
 
 router.post('/create', createDelivery);
-router.get('/:id', getDeliveryById);
-router.get('/order/:orderId', getDeliveryByOrderId);
-router.get('/driver/:driverId', getDriverDeliveries);
-router.put('/:id/pickup', updatePickupTime);
-router.put('/:id/complete', completeDelivery);
-
 
 export default router;
