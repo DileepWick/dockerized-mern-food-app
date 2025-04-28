6ac555ed2ea1437c969b856c23d7f44962b62a6c
Dileepa <113455403+dileepa200108@users.noreply.github.com>
2025-04-28 15:52:25 +0530

    updated

--- restaurant-service/utils/axiosInstances.js
+++ restaurant-service/utils/axiosInstances.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance of axios for the authentication service
 const authService = axios.create({
-  baseURL: 'http://localhost:3000/api', // Change this to http://localhost:3000 when dev
+  baseURL: 'http://auth-service/api', // Change this to http://localhost:3000 when dev
   withCredentials: true,
 });
 
