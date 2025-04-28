5e55d62fc09f3c1021aa95faf7ee21b8e73d4eb9
Dileepa <113455403+dileepa200108@users.noreply.github.com>
2025-04-28 17:46:38 +0530

    changed routes

--- order-service/utils/axiosInstances.js
+++ order-service/utils/axiosInstances.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 // Create an instance of axios for the authentication service
 const authService = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "http://auth-service:3000/api",
   withCredentials: true, 
 });
 
 // Create an instance of axios for the restaurant service
 const restaurantService = axios.create({
-  baseURL: "http://localhost:3005/api",
+  baseURL: "http://restaurant-service:3005/api",
   withCredentials: true, 
 });
 
